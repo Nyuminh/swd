26642646142d79d5d09eaabc2f9f3911e4857e8c
Nyuminh <minhdlhse180733@fpt.edu.vn>
2026-03-04 12:28:09 +0700

    ignore

--- obj/Debug/net8.0/swd.AssemblyInfo.cs
+++ obj/Debug/net8.0/swd.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("swd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+619506fc0e6bdceb060acbb2e54eea90114623d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("swd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("swd")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
